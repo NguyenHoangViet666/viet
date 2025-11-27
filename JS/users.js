190c4a25dac561a6e33c444e70ce3a04b1d0b442
NguyenHoangViet666 <viet17510@gmail.com>
2025-11-27 12:18:37 +0700

    Delete JS directory

--- JS/users.js
+++
@@ -1,6 +0,0 @@
-const adminUsers = [
-    { username: "admin1", password: "adminpass1" },
-    { username: "sudo", password: "rootpassword" },
-    { username: "sysadmin", password: "complexpass" },
-];
-console.log("users.js loaded. Admin accounts defined:", adminUsers.length);
